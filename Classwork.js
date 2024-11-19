3702f8ad34ad0c8aeea04a37124b035cce649b00
PogoreltsevaAnnaUKDIPZ <anna.i.pohoreltseva@ukd.edu.ua>
2024-11-20 01:28:02 +0200

    Update Classwork.js

--- Classwork.js
+++ Classwork.js
@@ -1,48 +1,31 @@
 const fs = require('node:fs');
-const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-// const arr = [1,4,7,9];
-// const student = {
-//     name: 'John Dow',
-//     age: 22,
-// };
 
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 fs.readFile('1.txt', 'utf8', (err, data) => {
-    if (err){
+    if (err) {
         console.error(err);
         return;
     }
     console.log(data);
-})
-
-const lastLetters = lorem.split(' ').map(word => {
-    const newWord = word.slice(0,-1) + word.slice(-1).toUpperCase();
-
-    return newWord
-}).join(' ');
-
-const arr = [];
-
-for(){
-    arr.push(lastLetters[-1]);
-}
+});
 
 
+const lastLetters = lorem.split(' ')
+    .map(word => {
 
-fs.writeFile('2.txt', arr.toString(), 'utf8', (err, data) => {
-    
-    if (err){
-    console.error(err);
-    return;
+        const cleanWord = word.replace(/[^\w]/g, '');
+
+        return cleanWord.slice(-1).toUpperCase();
+    })
+    .filter(letter => letter) 
+    .join(''); 
+
+
+fs.writeFile('lastLetters.txt', lastLetters, 'utf8', (err) => {
+    if (err) {
+        console.error(err);
+        return;
     }
-})
-
-
-
-
-
-// fs.writeFile('1.txt', lorem, 'utf8', (err) => {
-//     if (err){
-//         console.error(err);
-//     }
-// })
+    console.log('Файл lastLetters.txt успішно створено!');
+});
